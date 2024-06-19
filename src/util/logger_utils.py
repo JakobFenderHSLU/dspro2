2761bb5ad070cac08f62bfe7b991644fa0ac3a77
Jakob Fender <jakob.fender@hslu.ch>
2024-06-19 16:17:11 +0200

    update logger format

--- src/util/logger_utils.py
+++ src/util/logger_utils.py
@@ -4,7 +4,7 @@ import logging
 def init_logging(name: str, level: int = logging.DEBUG) -> logging.Logger:
     log = logging.getLogger(name)
     log.setLevel(level)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    formatter = logging.Formatter('%(asctime)s %(levelname)-8s %(name)-15s - [%(filename)s:%(lineno)d]: %(message)s')
 
     fh = logging.FileHandler(f'logs/app.log')
     fh.setLevel(level)
