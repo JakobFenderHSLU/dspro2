fc59e799988fd48aa1a5a3bdfd151df04938926b
Jakob Fender <jakob.fender@hslu.ch>
2024-06-24 16:46:40 +0200

    make model bigger

--- src/scratch/classifier.py
+++ src/scratch/classifier.py
@@ -14,7 +14,7 @@ class AudioClassifier(nn.Module):
         conv_layers = []
 
         # First Convolution Block with Relu and Batch Norm. Use Kaiming Initialization
-        self.conv1 = nn.Conv2d(2, 16, kernel_size=(5, 5), stride=(2, 2), padding=(2, 2))
+        self.conv1 = nn.Conv2d(2, 16, kernel_size=(7, 7), stride=(2, 2), padding=(2, 2))
         self.relu1 = nn.ReLU()
         self.bn1 = nn.BatchNorm2d(16)
         init.kaiming_normal_(self.conv1.weight, a=0.1)
@@ -22,32 +22,32 @@ class AudioClassifier(nn.Module):
         conv_layers += [self.conv1, self.relu1, self.bn1]
 
         # Second Convolution Block
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.conv2 = nn.Conv2d(16, 64, kernel_size=(5, 5), stride=(2, 2), padding=(1, 1))
         self.relu2 = nn.ReLU()
-        self.bn2 = nn.BatchNorm2d(32)
+        self.bn2 = nn.BatchNorm2d(64)
         init.kaiming_normal_(self.conv2.weight, a=0.1)
         self.conv2.bias.data.zero_()
         conv_layers += [self.conv2, self.relu2, self.bn2]
 
         # Second Convolution Block
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.conv3 = nn.Conv2d(64, 256, kernel_size=(5, 5), stride=(2, 2), padding=(1, 1))
         self.relu3 = nn.ReLU()
-        self.bn3 = nn.BatchNorm2d(64)
+        self.bn3 = nn.BatchNorm2d(256)
         init.kaiming_normal_(self.conv3.weight, a=0.1)
         self.conv3.bias.data.zero_()
         conv_layers += [self.conv3, self.relu3, self.bn3]
 
         # Second Convolution Block
-        self.conv4 = nn.Conv2d(64, 128, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1))
+        self.conv4 = nn.Conv2d(256, 1024, kernel_size=(5, 5), stride=(2, 2), padding=(1, 1))
         self.relu4 = nn.ReLU()
-        self.bn4 = nn.BatchNorm2d(128)
+        self.bn4 = nn.BatchNorm2d(1024)
         init.kaiming_normal_(self.conv4.weight, a=0.1)
         self.conv4.bias.data.zero_()
         conv_layers += [self.conv4, self.relu4, self.bn4]
 
         # Linear Classifier
         self.ap = nn.AdaptiveAvgPool2d(output_size=1)
-        self.lin = nn.Linear(in_features=128, out_features=num_classes)
+        self.lin = nn.Linear(in_features=1024, out_features=num_classes)
 
         # Wrap the Convolutional Blocks
         self.conv = nn.Sequential(*conv_layers)
